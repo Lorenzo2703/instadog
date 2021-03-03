9f61a9f2a37e47a608ba85719c9032c01f49db87
Lorenzo2703 <gialollog@gmail.com>
2021-03-03 17:44:58 +0100

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SecondComponent } from './second/second.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+
+@NgModule({
+  declarations: [AppComponent, HomeComponent, SecondComponent],
+  imports: [BrowserModule, AppRoutingModule, FontAwesomeModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
